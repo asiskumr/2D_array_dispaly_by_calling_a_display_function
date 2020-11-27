46963f2c413927ae0c6e4574048157f84a2d78c4
asiskumr <63651791+asiskumr@users.noreply.github.com>
2020-11-27 22:25:08 +0530

    Update 2D_array_function.c

--- 2D_array_function.c
+++ 2D_array_function.c
@@ -1,3 +1,8 @@
+
+//create a 2D array by taking input from the user.
+//Write a display function to print 
+//the content of this 2D array on the screen.
+
 #include<stdio.h>
 #include <stdlib.h>
 int n_row,n_column;
@@ -6,10 +11,10 @@ void display(int A[n_row][n_column]);
 
 void main(){
 
-     printf("how many Row\n");
+     printf("how many Row: \n");
      int row,col;
      scanf("%d",&row);
-     printf("how many Column\n");
+     printf("how many Column: \n");
      scanf("%d",&col);
      int A[row][col];
      for(int i=0;i<row; i++){
@@ -19,8 +24,9 @@ void main(){
               
           }
       }
-      //printf("%d",A[0][0]);
-      
+     // printf("%d",A[0][0]);
+      n_row=row;
+      n_column=col;
      display(A);
       
     
@@ -31,10 +37,11 @@ void display(int arr[n_row][n_column]){
     
     for(int i=0;i<n_row; i++){
           for(int j=0; j<n_column; j++){
-             printf("Element of row %d and column %d is: %d",i,j,arr[i][j]);
+             printf(" \t%d\t",arr[i][j]);
               
               
           }
+          printf("\n");
       }
       
 } 
